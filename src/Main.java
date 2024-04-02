dd10f2546030ae78a324998e573644fd3d525036
ezquieljesus14 <usr3763@salesianos-lacuesta.net>
2024-04-02 12:18:30 +0100

    scanner input & message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Que pasa mi niño!!!!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número");
+        int input = scanner.nextInt();
+        System.out.println("El numero es: "+input);
+
+        scanner.close();
     }
-
-
 }
